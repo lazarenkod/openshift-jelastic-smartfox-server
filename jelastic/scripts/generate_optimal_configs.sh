95a25ffe7f11c3ec8f83921c1e5cb0bc106b04c4
Ihor Kolodyuk <ik@jelastic.com>
2014-06-23 17:18:15 +0300

    update

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -6,13 +6,13 @@ SED=$(which sed);
 # memory optimizer for smartfox server
 #
 
-SMARTFOX_JAVAOPS_SCRIPT="${OPENSHIFT_SFS_DIR}.openshift/action_hooks/start";
+SMARTFOX_CONTROL_SCRIPT="${OPENSHIFT_SFS_DIR}/bin/control";
 
 [ -z "$XMS" ] && { XMS=32; }
 memory_total=`free -m | grep Mem | awk '{print $2}'`;
 [ -z "$XMX" ] && { let XMX=memory_total-35;  }
 
-$SED -i  "s/-Xms[0-9]*m/-Xms${XMS}m/g"  $SMARTFOX_JAVAOPS_SCRIPT;
-$SED -i  "s/-Xmx[0-9]*m/-Xmx${XMX}m/g"  $SMARTFOX_JAVAOPS_SCRIPT;
+$SED -i  "s/-Xms[0-9]*m/-Xms${XMS}m/g"  $SMARTFOX_CONTROL_SCRIPT;
+$SED -i  "s/-Xmx[0-9]*m/-Xmx${XMX}m/g"  $SMARTFOX_CONTROL_SCRIPT;
 
 
